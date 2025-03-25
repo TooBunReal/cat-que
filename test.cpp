64f0e6466ed8a15888fbc55c9a1889ecc9e04710
TooBunReal <89735990+TooBunReal@users.noreply.github.com>
2025-03-25 18:54:24 +0700

    Update EnumerateProcessesAndThreads()

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <iostream>
 
-int wmain() {
+void CreateNotepadProcess() {
     STARTUPINFOW si;
     PROCESS_INFORMATION pi;
     ZeroMemory(&si, sizeof(si));
@@ -26,40 +26,63 @@ int wmain() {
         &pi
     )) {
         std::wcerr << L"CreateProcess failed (" << GetLastError() << L").\n";
-        return 1;
+        return;
     }
 
+    std::wcout << L"Notepad launched. Process handle: " << pi.hProcess << std::endl;
     std::wcout << L"Notepad launched. PID: " << pi.dwProcessId << std::endl;
 
     std::wofstream logFile(L"process_log.txt");
     if (!logFile.is_open()) {
         std::wcerr << L"Could not open log file.\n";
-        return 1;
+        return;
     }
 
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE) {
-        std::wcerr << L"Failed to take process snapshot.\n";
-        return 1;
-    }
-
-    PROCESSENTRY32W pe;
-    pe.dwSize = sizeof(PROCESSENTRY32W);
-
-    if (Process32FirstW(hSnapshot, &pe)) {
-        do {
-            logFile << L"PID: " << pe.th32ProcessID << L" | Executable: " << pe.szExeFile << L"\n";
-        } while (Process32NextW(hSnapshot, &pe));
-    } else {
-        std::wcerr << L"Process32First failed.\n";
-    }
-
-    CloseHandle(hSnapshot);
+    logFile << L"Notepad PID: " << pi.dwProcessId << L"\n";
     logFile.close();
-    std::wcout << L"Process list saved to process_log.txt.\n";
 
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
+}
 
+void EnumerateProcessesAndThreads() {
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to take process snapshot. Error: " << GetLastError() << std::endl;
+        return;
+    }
+
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    std::cout << "Process Name\t\tPID\tThread Count" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+
+    if (Process32First(hSnapshot, &pe)) {
+        do {
+            int threadCount = 0;
+            HANDLE hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+            if (hThreadSnap != INVALID_HANDLE_VALUE) {
+                THREADENTRY32 te;
+                te.dwSize = sizeof(THREADENTRY32);
+                if (Thread32First(hThreadSnap, &te)) {
+                    do {
+                        if (te.th32OwnerProcessID == pe.th32ProcessID) {
+                            threadCount++;
+                        }
+                    } while (Thread32Next(hThreadSnap, &te));
+                }
+                CloseHandle(hThreadSnap);
+            }
+            std::wcout << pe.szExeFile << "\t" << pe.th32ProcessID << "\t" << threadCount << std::endl;
+        } while (Process32Next(hSnapshot, &pe));
+    }
+
+    CloseHandle(hSnapshot);
+}
+
+int wmain() {
+    CreateNotepadProcess();
+    EnumerateProcessesAndThreads();
     return 0;
 }
